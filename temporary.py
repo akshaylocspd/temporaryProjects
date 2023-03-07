7f9c473999d4febe2e087d8d526b1266f156e75d
akshay <akshayvairagade80@gmail.com>
2023-03-07 18:12:03 +0530

    akshay

--- temporary.py
+++ temporary.py
@@ -1,6 +1,11 @@
+
+from webdriver_manager.firefox import  DriverManager
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
-driver = webdriver.PhantomJS()
-driver.set_window_size(1120, 550)
-driver.get("https://duckduckgo.com/")
-print(driver.current_url)
-driver.quit()
+options = FirefoxOptions()
+options.add_argument("--headless")
+
+executable_path=DriverManager().install()
+driver = webdriver.Firefox(options=options,executable_path=executable_path)
+driver.get("https://pythonbasics.org")
+driver.close()
