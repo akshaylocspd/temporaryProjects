15868f860bb8f0779197964c8c3f025ca9187553
akshay <akshayvairagade80@gmail.com>
2023-03-07 16:33:12 +0530

    selenium chromium example

--- seleniumFireFox.py
+++ seleniumFireFox.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.service import Service
+
+from selenium.webdriver.chrome.options import Options
 try:
-    fireFoxOptions = webdriver.FirefoxOptions()
-    fireFoxOptions.add_argument('--headless')
-    fireFoxOptions.add_argument('--no-sandbox')
-    brower = webdriver.Firefox(options=fireFoxOptions)
+    options = Options()
+    # this parameter tells Chrome that
+    # it should be run without UI (Headless)
+    # options.headless = True
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    
+    executable_path=ChromeDriverManager(version="89.0.4389.114",chrome_type=ChromeType.CHROMIUM).install()
+    brower = webdriver.Chrome(options=options)
     brower.get('https://pythonbasics.org')
     print(brower.page_source)
 finally:
