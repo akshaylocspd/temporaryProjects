7a4b13a7ab9cb1eaad5d599b425917ac1469bd75
akshay <akshayvairagade80@gmail.com>
2023-03-07 16:56:40 +0530

    added executable path

--- seleniumFireFox.py
+++ seleniumFireFox.py
@@ -5,15 +5,16 @@ from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.chrome.options import Options
 try:
+    print ('hanuman please help')
     options = Options()
     # this parameter tells Chrome that
     # it should be run without UI (Headless)
     # options.headless = True
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    
+    # notsfasd
     executable_path=ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-    brower = webdriver.Chrome(options=options)
+    brower = webdriver.Chrome(options=options,executable_path=executable_path)
     brower.get('https://pythonbasics.org')
     print(brower.page_source)
 finally:
