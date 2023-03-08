adb30f37f93cbf81b3c6997a23d155ece24c49d0
akshay <akshayvairagade80@gmail.com>
2023-03-08 22:04:42 +0530

    no sand box

--- temporary.py
+++ temporary.py
@@ -1,11 +1,10 @@
-
-from webdriver_manager.firefox import  DriverManager
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
 options = FirefoxOptions()
 options.add_argument("--headless")
+options.add_argument('--no-sandbox')   
 
-executable_path=DriverManager().install()
-driver = webdriver.Firefox(options=options,executable_path=executable_path)
+# executable_path=DriverManager().install()
+driver = webdriver.Firefox(options=options)
 driver.get("https://pythonbasics.org")
 driver.close()
